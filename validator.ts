8faae7c5042472ef416494fd43cf6bff5184cdae
hamza <86945130+hmmmzaDev@users.noreply.github.com>
2024-08-28 14:14:41 +0500

    removed the deprecated function

--- validator.ts
+++ validator.ts
@@ -2,36 +2,6 @@
 import { Request, Response, NextFunction } from "express";
 
 /**
-Validate the input data.
-@param key - The request key to validate.
-@param inputArr - The input array to check.
-@returns An Express middleware function.
- */
-export default function validate(key = "body", inputArr: string[]) {
-    return (req: Request, res: Response, next: NextFunction) => {
-        if (!inputArr || inputArr.length === 0) {
-            return next();
-        }
-        let missedInputs: any[] = [];
-        req["validData"] = {};
-        inputArr.forEach((input) => {
-            if ([undefined, null, ""].includes(req[key][input])) {
-                missedInputs.push(input);
-            }
-            req["validData"][input] = req[key][input];
-        });
-        if (missedInputs.length > 0) {
-            return res.status(400).json({
-                status: "error",
-                message: "provide the correct parameters",
-                missedInputs
-            });
-        }
-        return next();
-    }
-}
-
-/**
  * Validates the inputs in the request object based on the provided input array and field.
  *
  * @param {Object} options - The options object.
